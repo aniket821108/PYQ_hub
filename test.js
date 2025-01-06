7aa5501656fd389d7c316a5cf2e686d8a5cca631
Aniket kumar <146999255+aniket821108@users.noreply.github.com>
2025-01-06 21:57:01 +0530

    Add files via upload
    1st version

---
+++ test.js
@@ -0,0 +1,19 @@
+// let gameNumber = 67;
+// let userNum = prompt("Enter your guess number: ");
+// while (userNum != gameNumber) {
+//     userNum = prompt("wrong guess , try again");
+ 
+// }
+// console.log("congratultion you guess it ");
+//string
+
+
+// let str="aniket "; //1st way 
+// let str1='Aniket kumar'; //2nd way
+let obj ={
+    name:"aniket",
+    age:20,
+    }; //object
+
+    let output=`the name of the student is ${obj.name} and age is ${obj.age}`; // this is string literal
+    console.log(output);
